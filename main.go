74fad65f903969623434ed36543c6cd86d98bf82
Naveen Marathi <158243661+naveenmarathi@users.noreply.github.com>
2026-03-15 21:47:22 +0530

    Update main.go

--- main.go
+++ main.go
@@ -5,32 +5,42 @@ import (
 	"net/http"
 )
 
+// Home Page
 func homePage(w http.ResponseWriter, r *http.Request) {
-	// Render the home html page from static folder
 	http.ServeFile(w, r, "static/home.html")
 }
 
+// Courses Page
 func coursePage(w http.ResponseWriter, r *http.Request) {
-	// Render the course html page
 	http.ServeFile(w, r, "static/courses.html")
 }
 
+// About Page
 func aboutPage(w http.ResponseWriter, r *http.Request) {
-	// Render the about html page
 	http.ServeFile(w, r, "static/about.html")
 }
 
+// Contact Page
 func contactPage(w http.ResponseWriter, r *http.Request) {
-	// Render the contact html page
 	http.ServeFile(w, r, "static/contact.html")
 }
 
 func main() {
 
+	// Root route (fixes 404 issue)
+	http.HandleFunc("/", homePage)
+
+	// Other routes
 	http.HandleFunc("/home", homePage)
 	http.HandleFunc("/courses", coursePage)
 	http.HandleFunc("/about", aboutPage)
 	http.HandleFunc("/contact", contactPage)
+
+	// Static files (CSS, JS, images if needed)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	log.Println("Server started at http://localhost:8080")
 
 	err := http.ListenAndServe("0.0.0.0:8080", nil)
 	if err != nil {
